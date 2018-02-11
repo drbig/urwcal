77273e4de8cb7ef32c6d4a153f26140498a5da1e
Piotr S. Staszewski <p.staszewski@gmail.com>
2018-02-11 14:51:58 +0100

    Fix order in webpack config files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
-const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
+const merge = require('webpack-merge');
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   plugins: [
